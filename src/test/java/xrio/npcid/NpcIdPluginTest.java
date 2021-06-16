ac6483afdd81dc5d6788bab2862b06c5617d542a
XrioBtw <33559295+XrioBtw@users.noreply.github.com>
2021-06-16 20:51:19 +0200

    Add test

---
+++ src/test/java/xrio/npcid/NpcIdPluginTest.java
@@ -0,0 +1,13 @@
+package xrio.npcid;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class NpcIdPluginTest
+{
+	public static void main(String[] args) throws Exception
+	{
+		ExternalPluginManager.loadBuiltin(NpcIdPlugin.class);
+		RuneLite.main(args);
+	}
+}
