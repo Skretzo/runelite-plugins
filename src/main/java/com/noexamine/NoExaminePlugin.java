2028b72d9a83baf88fb9240a6a5f5e37ff91f8ae
MoreBuchus <107762640+MoreBuchus@users.noreply.github.com>
2022-06-22 13:56:51 -0500

    Fixed messing with tagging
    Fixed the issues of no examine making it so you are not able to tag npcs/objects and also made it ignore removing examine when in the bank so it does not remove edit-tags.

--- src/main/java/com/noexamine/NoExaminePlugin.java
+++ src/main/java/com/noexamine/NoExaminePlugin.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import net.runelite.api.Client;
 import net.runelite.api.MenuAction;
 import net.runelite.api.MenuEntry;
-import net.runelite.api.events.MenuEntryAdded;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
@@ -33,19 +32,42 @@ public class NoExaminePlugin extends Plugin
 		return configManager.getConfig(NoExamineConfig.class);
 	}
 
-	@Subscribe
-	public void onMenuEntryAdded(MenuEntryAdded event)
-	{
-		MenuAction menuAction = MenuAction.of(event.getType());
+	private final ArrayListMultimap<String, Integer> optionIndexes = ArrayListMultimap.create();
 
-		if ((MenuAction.EXAMINE_ITEM_GROUND.equals(menuAction) && config.itemsGround()) ||
-			(MenuAction.EXAMINE_NPC.equals(menuAction) && config.npcs()) ||
-			(MenuAction.EXAMINE_OBJECT.equals(menuAction) && config.objects()) ||
-			(MenuAction.CC_OP_LOW_PRIORITY.equals(menuAction) && config.itemInventory() && EXAMINE.equals(event.getOption())))
+	private final Predicate<MenuEntry> filterMenuEntries = entry ->
+	{
+		MenuAction menuAction = MenuAction.of(entry.getType().getId());
+
+		return (!MenuAction.EXAMINE_ITEM_GROUND.equals(menuAction) || !config.itemsGround()) &&
+			(!MenuAction.EXAMINE_NPC.equals(menuAction) || !config.npcs()) &&
+			(!MenuAction.EXAMINE_OBJECT.equals(menuAction) || !config.objects()) &&
+			(!MenuAction.CC_OP_LOW_PRIORITY.equals(menuAction) || !config.itemInventory()
+				|| !EXAMINE.equals(entry.getOption()) || entry.getParam1() == WidgetInfo.BANK_ITEM_CONTAINER.getId());
+	};
+
+
+	private MenuEntry[] updateMenuEntries(MenuEntry[] menuEntries)
+	{
+		return Arrays.stream(menuEntries)
+			.filter(filterMenuEntries).sorted((o1, o2) -> 0)
+			.toArray(MenuEntry[]::new);
+	}
+
+	@Subscribe
+	public void onClientTick(ClientTick clientTick)
+	{
+		if (client.getGameState() == GameState.LOGGED_IN && !client.isMenuOpen())
 		{
 			MenuEntry[] menuEntries = client.getMenuEntries();
-			MenuEntry[] newMenuEntries = Arrays.copyOf(menuEntries, menuEntries.length - 1);
-			client.setMenuEntries(newMenuEntries);
+			int idx = 0;
+			optionIndexes.clear();
+			for (MenuEntry entry : menuEntries)
+			{
+				String option = Text.removeTags(entry.getOption()).toLowerCase();
+				optionIndexes.put(option, idx++);
+			}
 		}
+
+		client.setMenuEntries(updateMenuEntries(client.getMenuEntries()));
 	}
 }
