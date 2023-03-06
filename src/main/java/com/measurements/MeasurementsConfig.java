4613e016973d85eadcdbe92252a22d3c1f1d4f58
Skretzo <53493631+Skretzo@users.noreply.github.com>
2023-03-06 02:37:27 +0100

    Initial version

--- src/main/java/com/measurements/MeasurementsConfig.java
+++ src/main/java/com/measurements/MeasurementsConfig.java
@@ -3,11 +3,21 @@ package com.measurements;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
-import net.runelite.client.config.Range
+import net.runelite.client.config.Range;
 
 @ConfigGroup("measurements")
 public interface MeasurementsConfig extends Config
 {
+	@ConfigItem(
+		keyName = "copyToClipboard",
+		name = "Copy to clipboard",
+		description = "Whether to copy the distance to the clipboard when measured"
+	)
+	default boolean copyToClipboard()
+	{
+		return false;
+	}
+
 	@Range(
 		min = 1,
 		max = 2
@@ -15,10 +25,22 @@ public interface MeasurementsConfig extends Config
 	@ConfigItem(
 		keyName = "diagonalLength",
 		name = "Diagonal length",
-		description = "The length of a diagonal. Manhattan/taxicab distance has length 2, while Chebyshev/chessboard distance has length 1"
+		description = "The length of a diagonal in the distance measurements.<br>" +
+			"Manhattan/taxicab distance has diagonal length 2,<br>" +
+			"while Chebyshev/chessboard distance has diagonal length 1"
 	)
 	default int diagonalLength()
 	{
 		return 1;
+	}
+
+	@ConfigItem(
+		keyName = "outputFormat",
+		name = "Output format",
+		description = "The output format of distance measurements in the panel<br>Example:<br>{ID}#{index}\t{distance}"
+	)
+	default String outputFormat()
+	{
+		return "{ID}\t{distance}";
 	}
 }
--- src/main/java/com/measurements/MeasurementsPlugin.java
+++ src/main/java/com/measurements/MeasurementsPlugin.java
@@ -1,28 +1,24 @@
 package com.measurements;
 
-import com.google.common.base.Strings;
 import com.google.inject.Inject;
 import com.google.inject.Provides;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.List;
-import lombok.Getter;
 import net.runelite.api.Client;
+import net.runelite.api.KeyCode;
+import net.runelite.api.MenuAction;
+import net.runelite.api.MenuEntry;
 import net.runelite.api.NPC;
-import net.runelite.api.events.GameTick;
-import net.runelite.api.events.OverheadTextChanged;
-import net.runelite.api.events.WidgetClosed;
-import net.runelite.api.events.WidgetLoaded;
-import net.runelite.api.widgets.Widget;
-import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.api.Player;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.api.events.MenuEntryAdded;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
-import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.ClientToolbar;
 import net.runelite.client.ui.NavigationButton;
-import net.runelite.client.ui.overlay.OverlayManager;
 import net.runelite.client.util.ImageUtil;
 
 @PluginDescriptor(
@@ -32,6 +28,8 @@ import net.runelite.client.util.ImageUtil;
 )
 public class MeasurementsPlugin extends Plugin
 {
+	private static final String MEASURE_DISTANCE_OPTION = "Distance to";
+
 	private MeasurementsPanel pluginPanel;
 	private NavigationButton navigationButton;
 
@@ -56,7 +54,7 @@ public class MeasurementsPlugin extends Plugin
 	@Override
 	protected void startUp()
 	{
-		pluginPanel = new MeasurementsPanel(config);
+		pluginPanel = new MeasurementsPanel();
 
 		final BufferedImage icon = ImageUtil.loadImageResource(getClass(), "/panel_icon.png");
 
@@ -76,5 +74,76 @@ public class MeasurementsPlugin extends Plugin
 		clientToolbar.removeNavigation(navigationButton);
 		pluginPanel = null;
 		navigationButton = null;
+	}
+
+	@Subscribe
+	public void onMenuEntryAdded(MenuEntryAdded event)
+	{
+		if (!client.isKeyPressed(KeyCode.KC_SHIFT))
+		{
+			return;
+		}
+
+		MenuEntry[] newEntries = new MenuEntry[1];
+
+		if (event.getType() == MenuAction.EXAMINE_NPC.getId())
+		{
+			newEntries = new MenuEntry[2];
+			newEntries[1] = client.createMenuEntry(-1)
+				.setType(MenuAction.RUNELITE)
+				.setIdentifier(event.getIdentifier())
+				.setParam0(event.getActionParam0())
+				.setParam1(event.getActionParam1())
+				.setOption(MEASURE_DISTANCE_OPTION)
+				.setTarget(event.getTarget())
+				.onClick(this::measureDistance);
+		}
+
+		for (MenuEntry entry : client.getMenuEntries())
+		{
+			if (MenuAction.CANCEL.equals(entry.getType()))
+			{
+				newEntries[0] = entry;
+				break;
+			}
+		}
+
+		client.setMenuEntries(newEntries);
+	}
+
+	private void measureDistance(MenuEntry entry)
+	{
+		NPC npc = client.getCachedNPCs()[entry.getIdentifier()];
+		Player player = client.getLocalPlayer();
+		if (npc == null || player == null)
+		{
+			return;
+		}
+
+		String measurement = config.outputFormat()
+			.replace("{ID}", Integer.toString(npc.getId()))
+			.replace("{index}", Integer.toString(npc.getIndex()))
+			.replace("{distance}", Integer.toString(distanceBetween(player.getWorldLocation(), npc.getWorldLocation())));
+
+		pluginPanel.appendText(measurement);
+		if (config.copyToClipboard())
+		{
+			Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(measurement), null);
+		}
+	}
+
+	private int distanceBetween(WorldPoint start, WorldPoint end)
+	{
+		int diagonal = config.diagonalLength();
+
+		int dx = Math.abs(start.getX() - end.getX());
+		int dy = Math.abs(start.getY() - end.getY());
+
+		if (diagonal == 2)
+		{
+			return dx + dy;
+		}
+
+		return Math.max(dx, dy);
 	}
 }
